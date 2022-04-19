8ffac8f8226279478581fc1eaa69401845a4ff59
Marco Túlio Terra <marcotulio.mtt@gmail.com>
2022-04-19 01:24:55 -0400

    feat: finalized

--- src/main.js
+++ src/main.js
@@ -1,18 +1,88 @@
 import fetcher from "./fetcher";
 
+function createCard(suggestion) {
+  const classes = suggestion?.staff_favorite
+    ? {
+        containerClass:
+          "content__sugestions__content__container-item--favourite",
+        cardClass: "favourite",
+        badge: '<div class="badge">✨ Staff favorite</div>',
+      }
+    : {
+        containerClass: "",
+        cardClass: "",
+        badge: "",
+      };
+
+  const generateTag = (key = "", value = "") => {
+    const assets = {
+      toxicity: {
+        yes: "toxic.svg",
+        no: "pet.svg",
+      },
+      sun: {
+        high: "low-sun.svg",
+        low: "low-sun.svg",
+        no: "no-sun.svg",
+      },
+      water: {
+        rarely: "1-drop.svg",
+        regularly: "2-drops.svg",
+        daily: "3-drops.svg",
+      },
+    };
+
+    let filename = "";
+
+    if (key === "toxicity") {
+      filename = value ? assets.toxicity.yes : assets.toxicity.no;
+    } else {
+      filename = assets[key][value];
+    }
+
+    return `<img src="${"./src/images/icons/" + filename}" />`;
+  };
+
+  const keys = ["toxicity", "water", "sun"];
+
+  const icons = keys //
+    .map((key) => generateTag(key, suggestion[key]))
+    .join("");
+
+  const element = `
+    <div class="content__sugestions__content__container-item column ${classes.containerClass}">
+      <div class="card ${classes.cardClass}" style="flex-grow: 1">
+        ${classes.badge}
+        <img
+          src="${suggestion.url}"
+          alt="${suggestion.name}"
+          class="image"
+        />
+        <div class="footer container">
+          <h3 class="title column">${suggestion.name}</h3>
+          <div class="details column">
+            <span class="price">${suggestion.price}</span>
+            <div class="icons">
+              ${icons}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  return element;
+}
+
 function main() {
   const sunInput = document.querySelector("[name=sun]");
   const waterInput = document.querySelector("[name=water]");
   const petInput = document.querySelector("[name=pets]");
-  const suggestionContainer = document.querySelector(
-    "main.content .suggestions-container"
-  );
+  const suggestionContainer = document.querySelector(".content__sugestions");
   const suggestionContainerContent = document.querySelector(
-    "main.content .suggestions-container .suggestion-content"
+    ".content__sugestions__content__container"
   );
-  const noContentContainer = document.querySelector(
-    "main.content .nocontent-container"
-  );
+  const noContentContainer = document.querySelector(".content__nocontent");
 
   // fetch data
   async function handleFetchData(options) {
@@ -21,32 +91,6 @@ function main() {
     if (!shouldFetch) return null;
 
     return fetcher(options);
-  }
-
-  function createCard(suggestion) {
-    const isFavorite = suggestion?.staff_favorite
-      ? '<div class="badge">✨ Staff favorite</div>'
-      : "";
-
-    const element = `
-      <div class="card">
-        ${isFavorite}
-        <img
-          src="${suggestion.url}"
-          alt="${suggestion.name}"
-          class="image"
-        />
-        <div class="footer">
-          <span class="title">${suggestion.name}</span>
-          <span class="price">${suggestion.price}</span>
-          <div class="icons">
-            <img src="./src/images/icons/pet.svg" alt="Pet" />
-          </div>
-        </div>
-      </div>
-    `;
-
-    return element;
   }
 
   // update view
@@ -67,7 +111,8 @@ function main() {
 
     suggestionContainerContent.innerHTML = data
       .sort((item) => (item?.staff_favorite ? -1 : 1))
-      .map((item) => createCard(item));
+      .map((item) => createCard(item))
+      .join("");
   }
 
   // form handler
